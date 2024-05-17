82908949fbf06015e55f6d5af0c720d67f1f5917
LemonBlanc <83752129+BWBMaxime@users.noreply.github.com>
2024-05-17 19:08:45 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 4200;
+
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+});
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL || 'mongodb://mongodb:27017/db_user')
+   .then(() => {
+       console.log('Connected to MongoDB');
+   }).catch((error) => {
+       console.error('Error connecting to MongoDB', error);
+   });
